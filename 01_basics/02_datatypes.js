43f3bf7b1ee7b4c40c8553eb2959dbe2a8894796
Ashish Nautiyal <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-04-13 07:37:00 +0000

    Datatypes , use strict , readability , Primitve datatypes , typeof

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello") // we are using node js so this will not run
+
+console.log(3+
+    3); //code readability should be high 
+
+console.log("Hello");
+
+// Data Types
+
+let name = "ashish"
+let age = 23
+let isLoggedIn = false
+
+
+// Primitive Datatypes
+// number  => 2 to the power of 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined
+// symbol => unique 
+
+// Object
+
+console.log(typeof "hitesh")
+console.log(typeof age)
+console.log(typeof null) //this is showing object //error
+console.log(typeof undefined) // undefined
