93e0ee52066c3d6bf666628a350ea46da146fb69
Ashish Nautiyal <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-04-13 07:51:25 +0000

    conversion , two methods to check typeof , NaN ,

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,26 @@
+let score = "ashish"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); //NaN
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //true
+
+// 1 => ture; 0 => false;
+// "" => false
+// "ashish" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
