011c2852716b27991cbcbdcf7ddb0f3e1c866ab6
Ashish Nautiyal <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-04-22 08:39:06 +0000

    switch

---
+++ 04_advanced/02_switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 2;
+
+// switch (month) {
+//     case 1: 
+//         console.log("Jan");
+//         break;
+//     case 2: 
+//         console.log("Feb");
+//         // break;
+//     case 3: 
+//         console.log("Mar");
+//         break;
+//     case 4: 
+//         console.log("Apr");
+//         break;
+//     default:
+//         console.log("No Match");
+//         break;
+// }
+
