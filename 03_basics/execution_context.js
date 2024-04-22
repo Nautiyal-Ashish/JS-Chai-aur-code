a91f43a63b526b248e69b9f0111ab1e681591243
Ashish Nautiyal <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-04-22 07:49:41 +0000

    renamed

--- 03_basics/execution_context.js
+++
@@ -1,5 +0,0 @@
-// 1. global execution context
-// 2. function execution context
-
-// Memory creation phase -> Variable and function memory allocated 
-// execution phase -> value given to all things
