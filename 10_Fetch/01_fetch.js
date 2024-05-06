9cba8866c6f136ee73ef24e97bf1b8167b5f94fd
Ashun007 <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-05-06 09:15:17 +0530

    Fetch & Fetch API

---
+++ 10_Fetch/01_fetch.js
@@ -0,0 +1,8 @@
+fetch("https://api.github.com/users/Nautiyal-Ashish")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
